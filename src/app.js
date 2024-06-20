aa454b23a969087a9ef1349fbea9b0bd6aa2af51
Shafik Bahou <snb3000@gmail.com>
2024-06-20 01:31:55 +0000

    saved changes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,42 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generateRandomIcon = () => {
+  const icons = ["♠", "♣", "♥", "♦"];
+  return icons[Math.floor(Math.random() * icons.length)];
 };
+
+const generateRandomNumber = () => {
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  return numbers[Math.floor(Math.random() * numbers.length)];
+};
+
+let generateNewCard = () => {
+  let randomIcon = generateRandomIcon();
+  let icons = document.querySelectorAll(".card-icon");
+  icons.forEach((icon) => {
+    icon.innerHTML = randomIcon;
+    icon.style.color =
+      randomIcon === "♦" || randomIcon === "♥" ? "red" : "black";
+  });
+
+  document.querySelector("#number").innerHTML = generateRandomNumber();
+};
+
+document
+  .getElementById("generateButton")
+  .addEventListener("click", generateNewCard);
